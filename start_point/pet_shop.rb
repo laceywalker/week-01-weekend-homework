a6a2d0a3f4f3b3c62f7376c2526eb45fefb71869
lacey <hiu@example.com>
2018-11-23 13:47:40 +0000

    pass four tests

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -5,3 +5,9 @@ end
 def total_cash(petshop)
   return petshop[:admin][:total_cash]
 end
+
+def add_or_remove_cash(petshop, amount)
+  initial_total = petshop[:admin][:total_cash]
+  new_total = initial_total + amount
+  petshop[:admin][:total_cash] = new_total
+end
