122f65cb64a3555783c9998ce5391e20c4d7bb9f
lacey <hiu@example.com>
2018-11-23 13:50:36 +0000

    pass five tests

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -6,8 +6,12 @@ def total_cash(petshop)
   return petshop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(petshop, amount)
+def add_or_remove_cash(petshop, amount)  #create new variable to store previous total, then add in argument, then set new total 
   initial_total = petshop[:admin][:total_cash]
   new_total = initial_total + amount
   petshop[:admin][:total_cash] = new_total
+end
+
+def pets_sold(petshop)
+  return petshop[:admin][:pets_sold]
 end
